d155cc9a903124b293860c16307490426521d8d1
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-06-21 16:54:02 +0700

    Constructor

---
+++ InstanceConstructor/Constructor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstanceConstructor
+{
+  class Inventory
+  {
+    internal string Name;
+    internal string Color;
+    internal int Number;
+    internal int Year;
+
+    internal Inventory()
+    {
+      Console.WriteLine("You don't place any inventory");
+    }
+
+    internal Inventory(string n, string col, int num, int y)
+    {
+      Name = n.ToUpper();
+      Color = col.ToUpper();
+      Number = num;
+      Year = y;
+
+      Console.WriteLine("Adding inventory OK");
+    }
+  }
+
+  class Pythagoras
+  {
+    internal float C;
+    Pythagoras() { }
+    internal static Pythagoras Sum(float a, float b)
+    {
+      Pythagoras objResult = new Pythagoras();
+      float c;
+      c = (float)Math.Sqrt(a * a + b * b);
+      objResult.C = c;
+      return objResult;
+      
+    }
+  }
+}
---
+++ InstanceConstructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InstanceConstructor
+{
+  class Run
+  {
+    internal Run(){
+      // Overloading constructor 
+      Inventory I1 = new Inventory();
+      Inventory I2 = new Inventory(
+        n: "Bag",
+        col: "black",
+        num: 1,
+        y: 2019
+        );
+
+      Console.WriteLine($"Just saved { I2.Name } { I2.Color } { I2.Number } { I2.Year }");
+
+      // Non public constructor
+      float a = 3.1F;
+      float b = 4.7F;
+      Pythagoras objRes = Pythagoras.Sum(a, b);
+      Console.WriteLine($"Pythagoras sum result from  {a} and {b} is: { objRes.C }");
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Run R = new Run();
+    }
+  }
+}
